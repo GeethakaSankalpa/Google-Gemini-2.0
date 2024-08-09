c5a6cafc8d4da3468c493ead7400dd3b7c46fc2b
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-10 01:32:56 +0530

    Designed UI of the application

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div className='main'>
         <div className="nav">
-            <p> Gemini </p>
+            <p> HitMe </p>
             <img src={assets.user} alt=''/>
         </div>
 
@@ -13,6 +13,42 @@ const Main = () => {
             <div className="greet">
                 <p><span> Hello, Geethaka.</span></p>
                 <p> How can I help you today?</p>
+            </div>
+
+            <div className="cards">
+                <div className="card">
+                    <p> Suggest beautiful places to see an upcoming road trip </p>
+                    <img src={assets.compass_icon} alt=''/>
+                </div>
+
+                <div className="card">
+                    <p> Explain this code </p>
+                    <img src={assets.bulb_icon} alt=''/>
+                </div>
+
+                <div className="card">
+                    <p> what is ML </p>
+                    <img src={assets.message_icon} alt=''/>
+                </div>
+
+                <div className="card">
+                    <p> Improve code </p>
+                    <img src={assets.code_icon} alt=''/>
+                </div>
+            </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type='text' placeholder='Enter a prompt here'/>
+                    <div>
+                        <img src={assets.gallery_icon} alt=''/>
+                        <img src={assets.mic_icon} alt=''/>
+                        <img src={assets.send_icon} alt=''/>
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    HitMe may display inaccurate info, including about people, so double-check its responses. 
+                </p>
             </div>
         </div>
     </div>
