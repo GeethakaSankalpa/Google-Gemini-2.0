84a1ae87f5b482e74362ca8290ba853807148bcd
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-10 11:57:51 +0530

    update ** characters to bold  characters in response

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -13,6 +13,7 @@ const ContextProvider = (props) => {
     const[loading, setLoading] = useState(false);
     const[resultData, setResultData] = useState("");
     
+    const delayPara = (index, nextWord)=>{}
 
     const onSent = async (prompt)=>{
 
@@ -21,7 +22,16 @@ const ContextProvider = (props) => {
         setDisplayResult(true)
         setRecentPrompt(input)
         const response = await run(input)
-        setResultData(response)
+        let responseArray = response.split("**");
+        let newResponse;
+        for(let i=0; i < responseArray.length; i++){
+            if(i==0 || i%2 == 1){
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+        setResultData(newResponse)
         setLoading(false)
         setInput("")    // reset input
     }
